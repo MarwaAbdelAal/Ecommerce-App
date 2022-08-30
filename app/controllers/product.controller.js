93837b0fea55587f388e150455bcf62e35a2681b
marwaabdelaal <marwaabdelaal112@gmail.com>
2022-08-30 14:56:32 +0200

    added status res

--- app/controllers/product.controller.js
+++ app/controllers/product.controller.js
@@ -4,10 +4,10 @@ class Product{
     static getAllProducts = async(req, res)=>{ //localhost:3000/product/
         try {
             const data = await productModel.find()
-            res.send({apiStatus: true, message: "all products fetched", data})
+            res.status(200).send({apiStatus: true, message: "all products fetched", data})
         } 
         catch (e) {
-            res.send({apiStatus: false, message: e.message, data: e})
+            res.status(500).send({apiStatus: false, message: e.message, data: e})
         }
     }
     static addProduct = async(req, res)=>{
@@ -17,19 +17,19 @@ class Product{
                 ...req.body
             })
             await productData.save()
-            res.send({apiStatus: true, message: "product added", data: productData})
+            res.status(200).send({apiStatus: true, message: "product added", data: productData})
         } 
         catch (e) {
-            res.send({apiStatus: false, message: e.message, data: e})
+            res.status(500).send({apiStatus: false, message: e.message, data: e})
         }
     }
     static singleProduct = async(req, res)=>{
         try {
             const data = await productModel.findById(req.params.id)
-            res.send({apiStatus: true, message: "single Product", data})
+            res.status(200).send({apiStatus: true, message: "single Product", data})
         } 
         catch (e) {
-            res.send({apiStatus: false, message: e.message, data: e})
+            res.status(500).send({apiStatus: false, message: e.message, data: e})
         }
     }
     static editProduct = async(req, res)=>{
@@ -38,38 +38,38 @@ class Product{
                 req.params.id,
                 req.body, 
                 {runValidator:true})
-            res.send({apiStatus: true, message: "Product edited", data})
+            res.status(200).send({apiStatus: true, message: "Product edited", data})
         } 
         catch (e) {
-            res.send({apiStatus: false, message: e.message, data: e})
+            res.status(500).send({apiStatus: false, message: e.message, data: e})
         }
     }
     static delProduct = async(req, res)=>{
         try {
             const data = await productModel.findByIdAndDelete(req.params.id)
-            res.send({apiStatus: true, message: "Product deleted", data})
+            res.status(200).send({apiStatus: true, message: "Product deleted", data})
         } 
         catch (e) {
-            res.send({apiStatus: false, message: e.message, data: e})
+            res.status(500).send({apiStatus: false, message: e.message, data: e})
         }
     }
     static getMyProducts = async(req,res)=>{
         try {
             const data = await productModel.find({userId:req.user._id})
-            res.send({apiStatus: true, message: "My products fetched", data})
+            res.status(200).send({apiStatus: true, message: "My products fetched", data})
         } 
         catch (e) {
-            res.send({apiStatus: false, message: e.message, data: e})
+            res.status(500).send({apiStatus: false, message: e.message, data: e})
         }
 
     }    
     static myProducts = async(req,res)=>{
         try {
             await req.user.populate("myProducts")
-            res.send({apiStatus: true, message: "My products fetched", data: req.user.myProducts})
+            res.status(200).send({apiStatus: true, message: "My products fetched", data: req.user.myProducts})
         } 
         catch (e) {
-            res.send({apiStatus: false, message: e.message, data: e})
+            res.status(500).send({apiStatus: false, message: e.message, data: e})
         }
 
     }    
