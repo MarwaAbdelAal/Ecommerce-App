ed6a090ac0129ae208249a87fc2fc5d1cfc4941f
marwaabdelaal <marwaabdelaal112@gmail.com>
2022-08-30 14:24:07 +0200

    added cors

--- app/app.js
+++ app/app.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
+app.use(cors())
 require("./database/connect") //connect to database
 
 app.use(express.json()) // deal with json data
